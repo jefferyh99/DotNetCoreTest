3c37ede45c51a70f72249302592d1708c9d690d7
suixi.hong <suixi.hong@chukou1.com>
2018-10-11 18:31:44 +0800

    update:修改代码

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace MVC
 {
@@ -15,11 +16,33 @@ namespace MVC
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
 
-            //var serviceScope = host.Services.CreateScope();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("Config/nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
 
-            host.Run();
+                var host = CreateWebHostBuilder(args).Build();
+
+                //var serviceScope = host.Services.CreateScope();
+
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
+
+
 
             //通用主机，非web资源，如后台定时器等等
             //new HostBuilder().ConfigureHostConfiguration(configHost =>
@@ -30,34 +53,35 @@ namespace MVC
             //    configHost.AddCommandLine(args);
             //}).Build().Run();
 
-           
-
-
         }
 
         //web 主机
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 
-            // 默认配置有很多
+            // 默认配置有很多,默认是有appsettings.Development.json配置的
+            //config.AddJsonFile("appsettings.json", true, true).AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true);
             WebHost.CreateDefaultBuilder(args)
 
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-
-                    var abc = hostingContext.Configuration.GetSection("Logging");
-                    var result = abc.AsEnumerable();
-                    logging.ClearProviders();//清除默认配置
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logging.AddConsole();//不会有效果，因为已经在CreateDefaultBuilder中配置,需要清除默认才有效果
-                    logging.AddDebug();
-                    var logLevel = LogLevel.Critical;//日志级别
-
-                })
-                .UseKestrel()
-                .UseStartup<Startup>();
-
-
-
+            //自带默认log配置
+            //.ConfigureLogging((hostingContext, logging) =>
+            //{
+            //    var abc = hostingContext.Configuration.GetSection("Logging");
+            //    var result = abc.AsEnumerable();
+            //    logging.ClearProviders();//清除默认配置
+            //    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+            //    logging.AddConsole();//不会有效果，因为已经在CreateDefaultBuilder中配置,需要清除默认才有效果
+            //    logging.AddDebug();
+            //    var logLevel = LogLevel.Critical;//日志级别
+            //})
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();  // NLog: setup NLog for Dependency injection
+       
 
     }
 }
