3e523603de5d5998b8b7bcda98916c0b602a4d55
jeffhong <jeffhong99@163.com>
2019-03-27 23:05:58 +0800

    update:IdentityService

---
+++ IdentityService/Startup.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityService
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            InMemoryConfiguration.Configuration = this.Configuration;
+
+            services.AddIdentityServer()
+            .AddDeveloperSigningCredential()//生产环境时需要使用AddSigningCredential()
+            .AddTestUsers(InMemoryConfiguration.GetUsers().ToList())
+            .AddInMemoryClients(InMemoryConfiguration.GetClients())
+            .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources());
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+
+    /// <summary>
+    /// （1）哪些API可以使用这个AuthorizationServer
+    　　/// （2）哪些Client可以使用这个AuthorizationServer
+    　　/// （3）哪些User可以被这个AuthrizationServer识别并授权
+    /// </summary>
+    public class InMemoryConfiguration
+    {
+        public static IConfiguration Configuration { get; set; }
+        /// <summary>
+        /// Define which APIs will use this IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("clientservice", "CAS Client Service"),
+                new ApiResource("productservice", "CAS Product Service"),
+                new ApiResource("agentservice", "CAS Agent Service")
+            };
+        }
+
+        /// <summary>
+        /// Define which Apps will use thie IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client.api.service",
+                    ClientSecrets = new [] { new Secret("clientsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "clientservice" }
+                },
+                new Client
+                {
+                    ClientId = "product.api.service",
+                    ClientSecrets = new [] { new Secret("productsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "clientservice", "productservice" }
+                },
+                new Client
+                {
+                    ClientId = "agent.api.service",
+                    ClientSecrets = new [] { new Secret("agentsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "agentservice", "clientservice", "productservice" }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Define which uses will use this IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "10001",
+                    Username = "edison@hotmail.com",
+                    Password = "edisonpassword"
+                },
+                new TestUser
+                {
+                    SubjectId = "10002",
+                    Username = "andy@hotmail.com",
+                    Password = "andypassword"
+                },
+                new TestUser
+                {
+                    SubjectId = "10003",
+                    Username = "leo@hotmail.com",
+                    Password = "leopassword"
+                }
+            };
+        }
+    }
+
+
+}
